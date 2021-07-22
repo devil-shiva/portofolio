a4ee6b586423fbfc88ce4c614e360d22a2051f64
devil-shiva <shivamsaksham2000@gmail.com>
2021-07-22 12:34:56 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+// setTimeout(() => {
+    
+//     document.getElementById('loading').style.maxHeight = "0"
+
+// }, 3500);
+
+let closenav = document.querySelector('.closebtn')
+let ham = document.getElementById('hamburger')
+let nav = document.getElementById('nav')
+let open = false
+ham.addEventListener('click' , ()=>{
+    nav.style.maxWidth = "100%"
+        setTimeout(() => {
+            open =  true
+        }, 100);;
+
+})
+document.addEventListener('click', ()=>{
+    if (open) {
+        nav.style.maxWidth= '0'
+        open = false
+    }
+})
+
+closenav.addEventListener('click', ()=>{
+    nav.style.maxWidth = "0"
+    open = false;
+})
